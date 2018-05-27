24cda4825f924de8611df1b1694244cc7418b65e
Asseel <a.sidique@hotmail.com>
2018-05-27 03:51:32 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit bde4d1ad831c9e7add461a949d27032ea8d1158d, reversing
    changes made to 066e210220ae0965c42c818c70d0f1e2a53c65ba.

--- src/comp/Appointments/Appointments.js
+++ src/comp/Appointments/Appointments.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import './Appointments.css'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class Appointments extends React.Component {
   //constructor(props) {
   //  super(props);
   //}
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false
-    }
-    this.toggleModal = this.toggleModal.bind(this);
-    this.confirmAppointment = this.confirmAppointment.bind(this);
-
-  }
-  toggleModal() {
-    this.setState({modal: !this.state.modal});
-  }
-
-  confirmAppointment() {
-    this.toggleModal();
-    window.alert('Your appointment has been set.');
-  }
 
   render() {
     return(
@@ -48,22 +29,9 @@ class Appointments extends React.Component {
         <div className="card">
           <div className="card-header">Request New Appointment</div>
           <div className="card-body">
-          <button onClick={this.toggleModal} className="btn btn-success btn-left">Schedule</button>
+          <button className="btn btn-success btn-left">Schedule</button>
           </div>
         </div>
-        <Modal isOpen={this.state.modal} toggle={this.toggleModal} className={this.props.className}>
-          <ModalHeader toggle={this.toggleModal}>Schedule an appointment</ModalHeader>
-          <ModalBody>
-            <p>Enter date and time:</p>
-            <div className="input-group mb-3">
-              <input id="dateTimeIn" placeholder="Monday June 4, 2018 10:30 am"/>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.confirmAppointment}>Confirm</Button>
-            <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
       </div>
     )
   }
